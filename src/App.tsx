41902bbbf05264b0873c46f5f476176492d975ab
Ronen Begelfor <rbegelfor@rrc.ca>
2026-01-09 16:54:55 -0600

    Feat: Added header with application title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from "./components/common/footer/Footer";
 import './App.css'
+import Header from "./components/common/header/Header";
 
 function App() {
 
   return (
     <>
+      <Header />
       <Footer />
     </>
   )
