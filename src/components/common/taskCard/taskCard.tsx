72d5d634d6c5db38321d23bef76dde47ec72ec08
joshsxlud <jsalud2@academic.rrc.ca>
2026-01-19 21:39:10 -0600

    feat: updated the design of the task card component.

--- src/components/common/taskCard/taskCard.tsx
+++ src/components/common/taskCard/taskCard.tsx
@@ -7,15 +7,16 @@ starDifficulty();
 
 function TaskCard() {
     return (
-        <div>
             <div className="task-container">
                 {/*prop passed to fill  */}
-                <h1>Task Title</h1>
-                <div className="star">
-                    &#9733;
+                <div className="task-header-container">
+                    <h1>Sample Task</h1>
+                    <p>&#8942;</p> {/** TEMPORARY */}
                 </div>
+                    <div id="star">
+                        &#9733;  &#9733;  &#9733;  &#9733;
+                    </div>
                 <div className="task-description-container">
-                    <h2>Task Description</h2>
                     {/*prop passed to fill  */}
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
@@ -24,55 +25,15 @@ function TaskCard() {
                         Curabitur eget nisl id leo placerat ornare at eget lacus.                    
                     </p>
                     {/*prop passed to fill  */}
-                    <p>Assignee Name: Lorem Ipsum</p>
-                    {/*prop passed to fill  */}
-                    <p id="difficulty">Difficulty:</p>
-                </div>
-                
-            </div>
-            <div className="task-container">
-                {/*prop passed to fill  */}
-                <h1>Task Title</h1>
-                <div className="star">
-                    &#9733; &#9733; &#9733;
-                </div>
-                <div className="task-description-container">
-                    <h2>Task Description</h2>
-                    {/*prop passed to fill  */}
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Integer sed elit tempor, sodales massa eu, venenatis neque. 
-                        In hac habitasse platea dictumst. 
-                        Curabitur eget nisl id leo placerat ornare at eget lacus.                    
-                    </p>
-                    {/*prop passed to fill  */}
-                    <p>Assignee Name: Lorem Ipsum</p>
-                    {/*prop passed to fill  */}
-                    <p id="difficulty">Difficulty:</p>
+                    <div className="task-assignee-container">
+                        <p>John Smith</p>
+                    </div>
+                    <div className="task-date-container">
+                        {/*prop passed to fill  */}
+                        <p>Date: Lorem Ipsum</p>
+                    </div>
                 </div>
             </div>
-            <div className="task-container">
-                {/*prop passed to fill  */}
-                <h1>Task Title</h1>
-                <div className="star">
-                    &#9733; &#9733; &#9733; &#9733; &#9733;
-                </div>
-                <div className="task-description-container">
-                    <h2>Task Description</h2>
-                    {/*prop passed to fill  */}
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Integer sed elit tempor, sodales massa eu, venenatis neque. 
-                        In hac habitasse platea dictumst. 
-                        Curabitur eget nisl id leo placerat ornare at eget lacus.                    
-                    </p>
-                    {/*prop passed to fill  */}
-                    <p>Assignee Name: Lorem Ipsum</p>
-                    {/*prop passed to fill  */}
-                    <p id="difficulty">Difficulty:</p>
-                </div>
-            </div>
-        </div>
     )
 };
 
