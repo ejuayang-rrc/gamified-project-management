6b11691a8b2f6d6f51f11b75d34e2b2d422a8a63
ejuayang-rrc <ejuayang@rrc.ca>
2026-01-19 18:55:34 -0600

    Expanded navigation bar functionality.

--- src/components/common/nav/children/NavLinks.tsx
+++ src/components/common/nav/children/NavLinks.tsx
@@ -1,24 +1,48 @@
+import type { JSX } from "react";
 import "../Nav.css";
+
+type Page = {
+  id: number,
+  page: string
+};
+
+const placeholderPages: Page[] = [
+  {id: 0, page: "Home"},
+  {id: 1, page: "Placeholder"},
+  {id: 2, page: "Options"},
+  {id: 3, page: "Account"}
+];
 
 function Links() {
   return(
     <nav className="links">
-      <ul>
-        <li>      
-          <a href="/">Home</a>
-        </li> 
-        <li>   
-          <a href="/#placeholder">Placeholder</a>
-        </li> 
-        <li>  
-          <a href="/#options">Options</a>
-        </li> 
-        <li>
-          <a href="/#account">Account</a>
-        </li> 
-      </ul>
+      <DisplayLinks pages={placeholderPages}/>
     </nav>
   );
+}
+
+function DisplayLinks({pages}: {pages: Page[]}) {
+  const pageListItems: JSX.Element[] = [];
+
+  pages.forEach((page) => {
+    pageListItems.push(
+        <ListNavItem term={page.page} key={page.id}/>
+    );
+  })
+
+  return(
+    <ul>
+      {pageListItems}
+    </ul>
+  )
+}
+
+function ListNavItem({term}: {term: string}) {
+  return (
+    <li>
+      <a href="#">{term}</a>
+    </li>
+  )
 }
 
 export default Links;
