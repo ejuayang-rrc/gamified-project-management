e9f3ab73b750117adba184dd24b59d70514c9147
Ronen Begelfor <rbegelfor@rrc.ca>
2026-01-19 23:07:48 -0600

    Fix: Fixed star components issues.

--- src/components/common/starRating/starRating.tsx
+++ src/components/common/starRating/starRating.tsx
@@ -23,6 +23,8 @@ function starDifficulty() {
             const paragraphDifficulty = taskContainer?.querySelector('#difficulty');
 
             console.log(difficultyRating[count]);
+
+            //RB This is currently not used, as difficulty is set to be added later on when a task is expanded. This will be moved/adjusted accordingly.
             if (paragraphDifficulty?.textContent) {
                 paragraphDifficulty.textContent = `Difficulty: ${(difficultyRating[count]).charAt(0).toUpperCase() + (difficultyRating[count].slice(1))}`;
             }
