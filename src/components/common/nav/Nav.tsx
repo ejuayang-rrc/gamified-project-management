d8ed32eee974b8bb730e16d8b5da237229579fa7
ejuayang-rrc <ejuayang@rrc.ca>
2026-01-19 19:05:23 -0600

    Added comment to cite source for hamburger menu.

--- src/components/common/nav/Nav.tsx
+++ src/components/common/nav/Nav.tsx
@@ -2,6 +2,9 @@ import NavDesktop from "./children/NavDesktop";
 import NavMobile from "./children/NavMobile";
 import "./Nav.css";
 
+// Hamburger Menu Functionality taken from:
+// https://khuang159.medium.com/creating-a-hamburger-menu-in-react-f22e5ae442cb
+
 function Nav() {
   return(
     <div className="nav-element">
