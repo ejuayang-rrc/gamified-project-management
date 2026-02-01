a05193506b002ea7f1413951417d0948e1de8094
joshsxlud <jsalud2@academic.rrc.ca>
2026-02-01 14:38:04 -0600

    feat: installed tailwind and configured. tailwind implemented in the taskCard.

--- src/components/common/taskCard/taskCard.tsx
+++ src/components/common/taskCard/taskCard.tsx
@@ -7,18 +7,39 @@ starDifficulty();
 
 function TaskCard() {
     return (
-            <div className="task-container">
+            <div
+                className="
+                    bg-white
+                    shadow-[0.35rem_0.35rem_#545454]
+                    border border-[rgb(217,216,216)]
+                    rounded-[12px]
+                    p-8
+                    max-w-[20rem]
+                    text-black
+                    text-left
+                    m-0
+                
+            ">
                 {/*prop passed to fill  */}
-                <div className="task-header-container">
-                    <h1>Sample Task</h1>
+                <div className="
+                    flex
+                    justify-between
+                ">
+                    <h1 className="m-0">Sample Task</h1>
                     <p>&#8942;</p> {/** TEMPORARY */}
                 </div>
                     <div className="star">
                         &#9733;  &#9733;  &#9733;  &#9733;
                     </div>
-                <div className="task-description-container">
+                <div className="
+                    rounded-[9px]
+                    text-[#545454]
+                ">
                     {/*prop passed to fill  */}
-                    <p>
+                    <p className="
+                        m-0
+                        text-2xl
+                    ">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                         Integer sed elit tempor, sodales massa eu, venenatis neque. 
                         In hac habitasse platea dictumst. 
