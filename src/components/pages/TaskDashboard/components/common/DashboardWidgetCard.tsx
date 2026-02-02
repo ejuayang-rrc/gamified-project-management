3e4981db815287a73101397722c7d2711ab2219e
joshsxlud <jsalud2@academic.rrc.ca>
2026-02-02 16:44:32 -0600

    feat: created a common widgetCard.

---
+++ src/components/pages/TaskDashboard/components/common/DashboardWidgetCard.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+
+type DashboardWidgetCardProps = {
+    widgetTitle: string;
+
+    children: ReactNode;
+};
+
+const DashboardWidgetCard = ({widgetTitle, children}: DashboardWidgetCardProps) => {
+
+    return (
+        <div>
+            
+            <div>
+                <h3>{widgetTitle}</h3>
+            </div>
+            {/* Will render the content depending on the widget. */}
+            <div>{children}</div>
+
+        </div>
+    )
+}
