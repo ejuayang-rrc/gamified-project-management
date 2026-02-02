e1cc198279aa48b3e3775cab8a4169d9f5de5117
joshsxlud <jsalud2@academic.rrc.ca>
2026-02-02 16:18:55 -0600

    feat: created a basic layout to use on each page. Changes pushed to feat/star-refactor

---
+++ src/components/pages/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from "../../common/header/Header";
+import Footer from "../../common/footer/Footer";
+import { Outlet } from "react-router";
+
+// Layout To be used for each page.
+const layout = ({}) => {
+
+    return (
+        <>
+            <Header />
+                <Outlet />
+            <Footer />
+        </>
+    )
+}
+
+export default layout;
