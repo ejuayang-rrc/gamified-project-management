d554dc5e5f7c7d0e251cb246f81c51dd5cd8afa6
ejuayang-rrc <ejuayang@rrc.ca>
2026-02-02 21:38:35 -0600

    Created a SettingsDisplay common component.

---
+++ src/components/common/settingsDisplay/SettingsDisplay.tsx
@@ -0,0 +1,55 @@
+import type { JSX } from "react";
+import { useState } from "react";
+import type { UserSetting } from "../../types/setting";
+import { SettingCard } from "../settingCard/SettingCard";
+
+export function SettingsDisplay({
+  settings,
+  updateSettings
+}:{
+  settings: UserSetting[],
+  updateSettings: React.Dispatch<React.SetStateAction<UserSetting[]>>
+}) {
+  const [expandedTerm, setExpandedTerm] = useState<string|null>(null);
+  
+  const handleSettingsClick = (settingClicked: UserSetting): void => {
+    updateSettings(prevState => {
+      return prevState.map(s => {
+        // Change value boolean of setting
+        if(s.name === settingClicked.name) {
+          let newValue = !s.value;
+          return {...s, value: newValue};
+        } else {
+          return s;
+        }
+      })
+    });
+  }
+
+  const listSettings: JSX.Element[] = settings.map((setting) => {
+    return (
+      <SettingCard 
+        setting = {setting}
+        isExpanded = {setting.name === expandedTerm}
+
+        onTitleClick={() => {
+            setting.name !== expandedTerm ?
+            setExpandedTerm(setting.name) :
+            setExpandedTerm(null);
+        }}
+
+        onSaveClick = {() => {
+          handleSettingsClick(setting);
+        }}
+
+        key={setting.name} 
+      />
+    )
+  });
+
+  return(
+    <div>
+      {listSettings}
+    </div>
+  )
+}
