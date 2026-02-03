6eeaafa38dd950ef1383b797bc6a84bb893fd82c
ejuayang-rrc <ejuayang@rrc.ca>
2026-02-02 19:13:23 -0600

    Added temporary backend for settings storage.

---
+++ src/components/settings/settingsData.ts
@@ -0,0 +1,8 @@
+import type { UserSetting } from "../types/setting";
+
+/* PLACEHOLDER FOR BACKEND */
+
+export const settingsData: UserSetting[] = [
+  {name: "darkTheme", value: false},
+  {name: "horse", value: 0}
+]
