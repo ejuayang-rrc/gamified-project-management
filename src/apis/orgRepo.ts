dfa4d955f1b073471a59f7f06b8f809f03083acc
ejuayang-rrc <ejuayang@rrc.ca>
2026-03-02 20:17:05 -0600

    Created group management within organizations.

--- src/apis/orgRepo.ts
+++ src/apis/orgRepo.ts
@@ -24,23 +24,52 @@ export function addGroupToOrg(
   groupId: number
 ) {
   try {
+    const index: number = orgData.findIndex(
+      (organization: Organization) => organization.id === orgId
+    );
 
+    if (index === -1) {
+      throw new Error(`Org with ${orgId} cannot be found`);
+    }
+
+    orgData[index].groups.push(groupId);
+
+    return orgData;
   } catch (error: unknown) {
     throw error;
   }
 }
 
-export function removeGroupFromOrg() {
+export function removeGroupFromOrg(
+  orgId: number,
+  groupId: number
+) {
   try {
-    
+    const orgIndex: number = orgData.findIndex(
+      (organization: Organization) => organization.id === orgId
+    );
+
+    if (orgIndex === -1) {
+      throw new Error(`Org with ${orgId} cannot be found`);
+    } else {
+      const groupIndex: number = orgData[orgIndex].groups.findIndex(
+        (group: number) => group === groupId
+      );
+
+      if (orgIndex === -1) {
+        throw new Error(`Group with ${groupId} cannot be found`);
+      }
+
+      orgData[orgIndex].groups.splice(groupIndex, 1);
+    }
+
+    return orgData;
   } catch (error: unknown) {
     throw error;
   }
 }
 
-export function createOrganization(
-  orgName: string
-): Organization {
+export function createOrganization(orgName: string): Organization {
   try {
     let newId: number = 0;
     let isNotUnique: boolean = true;
@@ -73,13 +102,15 @@ export function createOrganization(
 
 export function deleteOrganization(orgId: number) {
   try {
-    const getOrg = orgData.find(entry => entry.id === orgId);
+    const index: number = orgData.findIndex(
+      (organization: Organization) => organization.id === orgId
+    );
 
-    if(!getOrg) {
+    if (index === -1) {
       throw new Error(`Org with ${orgId} cannot be found`);
     }
       
-    orgData.splice(orgId, 1);
+    orgData.splice(index, 1);
   } catch (error: unknown) {
     throw error;
   }
