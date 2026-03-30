7a94d55842339ab5d85193f513e8f954f44bc940
Ronen Begelfor <rbegelfor@rrc.ca>
2026-03-30 14:21:44 -0500

    feat: created a basic cors config, allowing only localhost.

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,10 +4,27 @@ import cors from "cors";
 // initialize express application
 const app: Express = express();
 
+//RB This is the temporary way for the allowed origin as we are currently running locally.
+const allowedOrigin = 'http://localhost:5173';
 
 // allow express to parse json
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+        
+        if (origin === allowedOrigin)
+        {
+            callback(null, true);
+        }
+    },
+    methods: [
+        'GET', 'POST', 'PUT', 'DELETE'
+    ]
+    
+}));
 
 // listen for requests on root and send simple text response
 app.get("/",  (_req, res) => {
